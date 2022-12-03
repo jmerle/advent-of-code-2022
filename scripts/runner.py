2aba33ff07ce7aa8bd8e89dc69a3bb6cdfcf2baa
Jasper van Merle <jaspervmerle@gmail.com>
2022-12-03 07:05:35 +0100

    Update scripts/runner.py to log message when data file is empty

--- scripts/runner.py
+++ scripts/runner.py
@@ -16,7 +16,7 @@ def main() -> None:
 
     process = None
     process_code_file = None
-    process_data_file = None
+    process_data_file_fd = None
 
     try:
         for changes in watch(project_root):
@@ -40,21 +40,25 @@ def main() -> None:
                 if process is not None and process.poll() is None:
                     print("Killing previous process")
                     process.kill()
-                    process_data_file.close()
+                    process_data_file_fd.close()
 
                 module_name = str(code_file.relative_to(project_root)).replace("/", ".").replace(".py", "")
-                data_file = (code_file.parent / args.data_file_name).open("r")
+                data_file = code_file.parent / args.data_file_name
+                data_file_fd = data_file.open("r")
 
                 print()
 
-                process = subprocess.Popen([sys.executable, "-m", module_name], cwd=project_root, stdin=data_file)
+                if len(data_file.read_text(encoding="utf-8")) == 0:
+                    print(f"{args.data_file_name} is empty!")
+
+                process = subprocess.Popen([sys.executable, "-m", module_name], cwd=project_root, stdin=data_file_fd)
                 process_code_file = code_file
-                process_data_file = data_file
+                process_data_file_fd = data_file_fd
     except KeyboardInterrupt:
         if process is not None and process.poll() is None:
             print("Killing current process")
             process.kill()
-            process_data_file.close()
+            process_data_file_fd.close()
 
         sys.exit(1)
 
